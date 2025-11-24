2cac87f48fa18e679741811e9e7228d6af74a665
ifthenuk <permana.ifuk@gmail.com>
2025-11-25 04:16:49 +0700

    change button use components

--- resources/views/app/users/index.blade.php
+++ resources/views/app/users/index.blade.php
@@ -7,27 +7,13 @@
         <div class="sm:flex">
 
             <div class="flex items-center ml-auto mb-4 space-x-2 sm:space-x-3">
-                <a href="{{ route('users.create') }}"
-                    class="inline-flex items-center  text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none">
-
-                    <svg class="w-6 h-6 me-1.5 -ms-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                        width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd"
-                            d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z"
-                            clip-rule="evenodd" />
-                    </svg>
+                <x-buttons.default href="{{ route('users.create') }}" variant="primary" pill iconLeft="fwb-s-user-plus">
                     Add User
-                </a>
-                <a href="#"
-                    class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
-                    <svg class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
-                            clip-rule="evenodd"></path>
-                    </svg>
+                </x-buttons.default>
+                
+                <x-buttons.default href="#" variant="secondary" pill iconLeft="fwb-s-download">
                     Export
-                </a>
+                </x-buttons.default>                
             </div>
         </div>
 
@@ -38,9 +24,7 @@
                         <label for="simple-search" class="sr-only">Search</label>
                         <div class="relative w-full">
                             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                                </svg>
+                                <x-fwb-o-search class="w-4 h-4 text-gray-500 dark:text-gray-400" />
                             </div>
                             <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
                         </div>
@@ -92,7 +76,7 @@
                             <tr class="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
                                 <td class="px-6 py-4" colspan="5">
                                     <p class="text-sm font-semibold font-monserrat text-center">
-                                        Não há usuarios cadastrados.
+                                        Data not found.
                                     </p>
                                 </td>
                             </tr>
