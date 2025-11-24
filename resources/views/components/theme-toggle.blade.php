b7752941987ae2daf0cfa2336e62efb9ec342126
ifthenuk <permana.ifuk@gmail.com>
2025-11-25 03:47:18 +0700

    update toggle theme

--- resources/views/components/theme-toggle.blade.php
+++ resources/views/components/theme-toggle.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button @click="darkMode = !darkMode" type="button"
+    <button @click="darkMode = !darkMode; localStorage.setItem('theme', darkMode ? 'dark' : 'light');" type="button"
         class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 
                focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 
                rounded-lg text-sm p-2.5">
