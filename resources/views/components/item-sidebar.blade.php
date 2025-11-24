0ebcc27243982aab40bc61b58ac5d44c3d195498
ifthenuk <permana.ifuk@gmail.com>
2025-11-25 03:46:59 +0700

    add drop down transition

--- resources/views/components/item-sidebar.blade.php
+++ resources/views/components/item-sidebar.blade.php
@@ -38,6 +38,10 @@
             x-collapse
             class="space-y-2 py-2"
             id={{ $id }}
+            x-transition:enter="transition ease-out duration-300"
+            x-transition:enter-start="opacity-0 -translate-y-2" x-transition:enter-end="opacity-100 translate-y-0"
+            x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0"
+            x-transition:leave-end="opacity-0 -translate-y-2" 
         >
             @foreach($submenu as $item)
                 <li>
